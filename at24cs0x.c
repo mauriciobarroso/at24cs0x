53f9db06733c3866d02cc0125b15539815af54e8
mauriciobarroso <mauriciobarroso1990@gmail.com>
2023-07-17 12:00:07 -0400

    implemented init and read functions

--- at24cs0x.c
+++ at24cs0x.c
@@ -34,6 +34,8 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "at24cs0x.h"
+#include "esp_err.h"
+#include "esp_log.h"
 
 /* Private macros ------------------------------------------------------------*/
 
@@ -42,10 +44,133 @@
 /* Private typedef -----------------------------------------------------------*/
 
 /* Private variables ---------------------------------------------------------*/
+static const char *TAG = "at24cs0x";
 
 /* Private function prototypes -----------------------------------------------*/
 
 /* Exported functions definitions --------------------------------------------*/
+/**
+ * @brief Function to initialize a AT24CS0x instance
+ */
+esp_err_t at24cs0x_init(at24cs0x_t *const me, i2c_bus_t *i2c_bus,
+		uint8_t dev_addr, i2c_bus_read_t read, i2c_bus_write_t write) {
+	/* Print initializing message */
+	ESP_LOGI(TAG, "Initializing AT24CS0X instance...");
+
+	/* Variable to return */
+	esp_err_t ret = ESP_OK;
+
+	/* Add device to bus */
+	ret = i2c_bus_add_dev(i2c_bus, dev_addr, "at24cs0x", NULL, NULL);
+
+	if (ret != ESP_OK) {
+		ESP_LOGE(TAG, "Failed to add device");
+		return ret;
+	}
+
+	/**/
+	me->i2c_dev = &i2c_bus->devs.dev[i2c_bus->devs.num - 1];
+
+	/* Print successful initialization message */
+	ESP_LOGI(TAG, "AT24CS0X instance initialized successfully");
+
+	/* Return ESP_OK */
+	return ret;
+}
+
+/**
+ * @brief Function to write a byte
+ */
+esp_err_t at24cs0x_write_byte(at24cs0x_t *const me, uint8_t data_addr, uint8_t *data) {
+	/* Variable to return */
+	esp_err_t ret = ESP_OK;
+
+	int8_t rslt = me->i2c_dev->write(data_addr, data, 1, me->i2c_dev);
+
+	if (rslt != I2C_BUS_OK) {
+		ESP_LOGE(TAG, "Failed to write a byte in 0x%02X", data_addr);
+		return ESP_FAIL;
+	} else {
+			printf("Error %d\n", rslt);
+	}
+
+	/* Return ESP_OK */
+	return ret;
+}
+
+/**
+ * @brief Function to write a page of 8 bytes
+ */
+esp_err_t at24cs0x_write_page(at24cs0x_t *const me, uint8_t data_addr, uint8_t *data) {
+	/* Variable to return */
+	esp_err_t ret = ESP_OK;
+
+	/* Return ESP_OK */
+	return ret;
+}
+
+/**
+ * @brief Function to read the current address
+ */
+esp_err_t at24cs0x_read_current(at24cs0x_t *const me, uint8_t data_addr, uint8_t *data) {
+	/* Variable to return */
+	esp_err_t ret = ESP_OK;
+
+
+	/* Return ESP_OK */
+	return ret;
+}
+
+/**
+ * @brief Function to read the current address
+ */
+esp_err_t at24cs0x_read_random(at24cs0x_t *const me, uint8_t data_addr, uint8_t *data) {
+	/* Variable to return */
+	esp_err_t ret = ESP_OK;
+
+	/**/
+	int8_t rslt = me->i2c_dev->read(data_addr, data, 1, me->i2c_dev);
+
+	if (rslt != I2C_BUS_OK) {
+		ESP_LOGE(TAG, "Failed to read a byte in 0x%02X", data_addr);
+		return ESP_FAIL;
+	}
+
+	/* Return ESP_OK */
+	return ret;
+}
+
+/**
+ * @brief Function to read the current address
+ */
+esp_err_t at24cs0x_read_sequential(at24cs0x_t *const me, uint8_t data_addr, uint8_t *data) {
+	/* Variable to return */
+	esp_err_t ret = ESP_OK;
+
+	/* Return ESP_OK */
+	return ret;
+}
+
+/**
+ * @brief Function to read the current address
+ */
+esp_err_t at24cs0x_read_serial_number(at24cs0x_t *const me) {
+	/* Variable to return */
+	esp_err_t ret = ESP_OK;
+
+	/**/
+	me->i2c_dev->addr = AT24CS0X_SN_I2C_ADDRESS; /* todo: write function to change the device address */
+	int8_t rslt = me->i2c_dev->read(0x80, me->serial_number, AT24CS0X_SN_SIZE, me->i2c_dev);
+	me->i2c_dev->addr = AT24CS0X_I2C_ADDRESS;
+
+	if (rslt != I2C_BUS_OK) {
+		ESP_LOGE(TAG, "Failed to read serial number");
+		return ESP_FAIL;
+	}
+
+	/* Return ESP_OK */
+	return ret;
+}
 
 /* Private function definitions ----------------------------------------------*/
 
--- include/at24cs0x.h
+++ include/at24cs0x.h
@@ -43,12 +43,112 @@ extern "C" {
 /* Includes ------------------------------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/timers.h"
+
+#include "driver/i2c.h"
+#include "i2c_bus.h"
 
 /* Exported Macros -----------------------------------------------------------*/
+#define AT24CS0X_SN_SIZE				(16) /*!< Serial number size */
+
+#define AT24CS0X_I2C_ADDRESS		0x50 /*!< Slave address for AT24CS0X */
+#define AT24CS0X_SN_I2C_ADDRESS	0x58 /*!< Slave address for AT24CS0X */
 
 /* Exported typedef ----------------------------------------------------------*/
 
+/*
+ * @brief AT24CS0x device structure
+ */
+typedef struct {
+	i2c_bus_dev_t *i2c_dev;
+	uint8_t serial_number[AT24CS0X_SN_SIZE];
+} at24cs0x_t;
+
 /* Exported variables --------------------------------------------------------*/
+/**
+ * @brief Function to initialize a AT24CS0x instance
+ *
+ * @param me      : Pointer to a structure instance of at24cs0x_t
+ * @param i2c_bus : Pointer to a structure with the data to initialize the
+ * 								    	   sensor as a I2C device
+ * @param addr    : I2C device address
+ * @param read    : I2C read function pointer
+ * @param write   : I2C write function pointer
+ *
+ * @return ESP_OK on success
+ */
+esp_err_t at24cs0x_init(at24cs0x_t *const me, i2c_bus_t *i2c_bus,
+		uint8_t dev_addr, i2c_bus_read_t read, i2c_bus_write_t write);
+
+/**
+ * @brief Function to write a byte
+ *
+ * @param me        : Pointer to a structure instance of at24cs0x_t
+ * @param data_addr : Address to write data
+ * @param data      : Data to write
+ *
+ * @return ESP_OK on success
+ */
+esp_err_t at24cs0x_write_byte(at24cs0x_t *const me, uint8_t data_addr, uint8_t *data);
+
+/**
+ * @brief Function to write a page of 8 bytes
+ *
+ * @param me        : Pointer to a structure instance of at24cs0x_t
+ * @param data_addr : Address to write data
+ * @param data      : Data to write
+ *
+ * @return ESP_OK on success
+ */
+esp_err_t at24cs0x_write_page(at24cs0x_t *const me, uint8_t data_addr, uint8_t *data);
+
+/**
+ * @brief Function to read the current address
+ *
+ * @param me        : Pointer to a structure instance of at24cs0x_t
+ * @param data_addr : Address to write data
+ * @param data      : Data to write
+ *
+ * @return ESP_OK on success
+ */
+esp_err_t at24cs0x_read_current(at24cs0x_t *const me, uint8_t data_addr, uint8_t *data);
+
+/**
+ * @brief Function to read the current address
+ *
+ * @param me        : Pointer to a structure instance of at24cs0x_t
+ * @param data_addr : Address to write data
+ * @param data      : Data to write
+ *
+ * @return ESP_OK on success
+ */
+esp_err_t at24cs0x_read_random(at24cs0x_t *const me, uint8_t data_addr, uint8_t *data);
+
+/**
+ * @brief Function to read the current address
+ *
+ * @param me        : Pointer to a structure instance of at24cs0x_t
+ * @param data_addr : Address to write data
+ * @param data      : Data to write
+ *
+ * @return ESP_OK on success
+ */
+esp_err_t at24cs0x_read_sequential(at24cs0x_t *const me, uint8_t data_addr, uint8_t *data);
+
+/**
+ * @brief Function to read the current address
+ *
+ * @param me        : Pointer to a structure instance of at24cs0x_t
+ * @param data_addr : Address to write data
+ * @param data      : Data to write
+ *
+ * @return ESP_OK on success
+ */
+esp_err_t at24cs0x_read_serial_number(at24cs0x_t *const me);
 
 /* Exported functions prototypes ---------------------------------------------*/
 
